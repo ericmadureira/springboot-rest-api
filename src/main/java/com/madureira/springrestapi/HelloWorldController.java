c76fdc92105368dd2774604220e9e55200e34f68
Eric Madureira <ericmadu416@gmail.com>
2024-03-07 17:28:42 -0300

    first commit

---
+++ src/main/java/com/madureira/springrestapi/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.madureira.springrestapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "Hello, Spring Boot admirers!";
+    }
+}

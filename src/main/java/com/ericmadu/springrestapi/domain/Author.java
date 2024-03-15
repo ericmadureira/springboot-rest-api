5da4cfabb9ebfb0083f13ac42cc2d7dc02228a72
Eric Madureira <ericmadu416@gmail.com>
2024-03-15 16:57:24 -0300

    feat: Author and Book classes for entities

---
+++ src/main/java/com/ericmadu/springrestapi/domain/Author.java
@@ -0,0 +1,19 @@
+package com.ericmadu.springrestapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Author {
+    // Long defaults to null, while long defaults to 0.
+    private Long id;
+    private String name;
+    private Integer age;
+
+
+}

dc316639c502b541e779cebcf14fe845bbead618
Eric Madureira <ericmadu416@gmail.com>
2024-03-15 18:39:45 -0300

    feat: Book DAO and impl

--- src/main/java/com/ericmadu/springrestapi/dao/BookDao.java
+++ src/main/java/com/ericmadu/springrestapi/dao/BookDao.java
@@ -1,4 +1,7 @@
 package com.ericmadu.springrestapi.dao;
 
+import com.ericmadu.springrestapi.domain.Book;
+
 public interface BookDao {
+    void create(Book book);
 }
--- src/main/java/com/ericmadu/springrestapi/dao/impl/BookDaoImpl.java
+++ src/main/java/com/ericmadu/springrestapi/dao/impl/BookDaoImpl.java
@@ -1,6 +1,8 @@
 package com.ericmadu.springrestapi.dao.impl;
 
 import com.ericmadu.springrestapi.dao.BookDao;
+import com.ericmadu.springrestapi.domain.Author;
+import com.ericmadu.springrestapi.domain.Book;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class BookDaoImpl implements BookDao {
@@ -8,5 +10,12 @@ public class BookDaoImpl implements BookDao {
 
     public BookDaoImpl(final JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(Book book) {
+        jdbcTemplate.update("INSERT INTO books (isbn, title, authorId) VALUES (?, ?, ?)",
+                book.getIsbn(), book.getTitle(), book.getAuthorId()
+        );
     }
 }

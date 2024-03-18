01091c766f42c2bda3f85698637916a9c5b217a2
Eric Madureira <ericmadu416@gmail.com>
2024-03-18 15:34:11 -0300

    feat: Author find many

--- src/main/java/com/ericmadu/springrestapi/dao/AuthorDao.java
+++ src/main/java/com/ericmadu/springrestapi/dao/AuthorDao.java
@@ -2,10 +2,13 @@ package com.ericmadu.springrestapi.dao;
 
 import com.ericmadu.springrestapi.domain.Author;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AuthorDao {
     void create(Author author);
 
     Optional<Author> findOne(long authorId);
+
+    List<Author> find();
 }

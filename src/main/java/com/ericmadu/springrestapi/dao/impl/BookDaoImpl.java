2368af76ea891616a025d29f96d9399c93b1070f
Eric Madureira <ericmadu416@gmail.com>
2024-03-18 14:52:26 -0300

    feat: fix column name case

--- src/main/java/com/ericmadu/springrestapi/dao/impl/BookDaoImpl.java
+++ src/main/java/com/ericmadu/springrestapi/dao/impl/BookDaoImpl.java
@@ -4,12 +4,14 @@ import com.ericmadu.springrestapi.dao.BookDao;
 import com.ericmadu.springrestapi.domain.Book;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public class BookDaoImpl implements BookDao {
     private final JdbcTemplate jdbcTemplate;
 
@@ -19,14 +21,15 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public void create(Book book) {
-        jdbcTemplate.update("INSERT INTO books (isbn, title, authorId) VALUES (?, ?, ?)",
+        // Pay ATTENTION to column name format. Must be the same as in schema and DB (author_id !== authorId).
+        jdbcTemplate.update("INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)",
                 book.getIsbn(), book.getTitle(), book.getAuthorId()
         );
     }
 
     @Override
     public Optional<Book> findOne(String isbn) {
-        List<Book> results = jdbcTemplate.query("SELECT isbn, title, authorId FROM books WHERE isbn = ? LIMIT 1",
+        List<Book> results = jdbcTemplate.query("SELECT isbn, title, author_id FROM books WHERE isbn = ? LIMIT 1",
                 new BookRowMapper(),
                 isbn
         );
@@ -39,7 +42,7 @@ public class BookDaoImpl implements BookDao {
             return Book.builder()
                     .isbn(rs.getString("isbn"))
                     .title(rs.getString("title"))
-                    .authorId(rs.getLong("authorId"))
+                    .authorId(rs.getLong("author_id"))
                     .build();
         }
     }

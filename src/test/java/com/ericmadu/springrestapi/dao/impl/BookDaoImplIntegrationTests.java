e2916578d96481b2b3a3b7e2294b1c3dda2d8e64
Eric Madureira <ericmadu416@gmail.com>
2024-03-18 14:54:42 -0300

    test: BookDaoImplIntegrationTests

---
+++ src/test/java/com/ericmadu/springrestapi/dao/impl/BookDaoImplIntegrationTests.java
@@ -0,0 +1,41 @@
+package com.ericmadu.springrestapi.dao.impl;
+
+import com.ericmadu.springrestapi.TestDataUtil;
+import com.ericmadu.springrestapi.dao.AuthorDao;
+import com.ericmadu.springrestapi.domain.Author;
+import com.ericmadu.springrestapi.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class BookDaoImplIntegrationTests {
+
+    private AuthorDao authorDao;
+    private BookDaoImpl underTest;
+
+    @Autowired
+    public BookDaoImplIntegrationTests(BookDaoImpl underTest, AuthorDao authorDao) {
+        this.underTest = underTest;
+        this.authorDao = authorDao;
+    }
+
+    @Test
+    public void testThatBookCanBeCreatedAndRecalled() {
+        Author author = TestDataUtil.createTestAuthor();
+        authorDao.create(author);
+        Book book = TestDataUtil.createTestBook();
+        book.setAuthorId(author.getId());
+        underTest.create(book);
+        Optional<Book> result = underTest.findOne("1111-0000-1111");
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(book);
+    }
+}

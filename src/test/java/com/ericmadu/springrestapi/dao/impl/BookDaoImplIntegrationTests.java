be871f7bcaa3cd27b53e75198f8c821fab46db9e
Eric Madureira <ericmadu416@gmail.com>
2024-03-19 15:27:25 -0300

    test: testThatMultipleBooksCanBeCreatedAndRecalled

--- src/test/java/com/ericmadu/springrestapi/dao/impl/BookDaoImplIntegrationTests.java
+++ src/test/java/com/ericmadu/springrestapi/dao/impl/BookDaoImplIntegrationTests.java
@@ -8,13 +8,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ExtendWith(SpringExtension.class)
 public class BookDaoImplIntegrationTests {
 
@@ -37,5 +40,28 @@ public class BookDaoImplIntegrationTests {
         Optional<Book> result = underTest.findOne("1111-0000-1111");
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(book);
+    }
+
+    @Test
+    public void testThatMultipleBooksCanBeCreatedAndRecalled() {
+        Author author = TestDataUtil.createTestAuthorA();
+        authorDao.create(author);
+
+        Book bookA = TestDataUtil.createTestBookA();
+        bookA.setAuthorId(author.getId());
+        underTest.create(bookA);
+
+        Book bookB = TestDataUtil.createTestBookB();
+        bookB.setAuthorId(author.getId());
+        underTest.create(bookB);
+
+        Book bookC = TestDataUtil.createTestBookC();
+        bookC.setAuthorId(author.getId());
+        underTest.create(bookC);
+
+        List<Book> result = underTest.find();
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(bookA, bookB, bookC);
     }
 }

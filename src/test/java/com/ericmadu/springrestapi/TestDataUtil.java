dff697031904c37c55aeea2f5c44c7a36c26d6f9
Eric Madureira <ericmadu416@gmail.com>
2024-03-19 11:08:11 -0300

    test: more book mocks

--- src/test/java/com/ericmadu/springrestapi/TestDataUtil.java
+++ src/test/java/com/ericmadu/springrestapi/TestDataUtil.java
@@ -31,11 +31,27 @@ public final class TestDataUtil {
                 .build();
     }
 
-    public static Book createTestBook() {
+    public static Book createTestBookA() {
         return Book.builder()
                 .isbn("1111-0000-1111")
                 .title("Memoirs of a noob programmer")
                 .authorId(1L)
+                .build();
+    }
+
+    public static Book createTestBookB() {
+        return Book.builder()
+                .isbn("2222-0000-2222")
+                .title("Out of Engineering Despair")
+                .authorId(2L)
+                .build();
+    }
+
+    public static Book createTestBookC() {
+        return Book.builder()
+                .isbn("3333-0000-3333")
+                .title("Meaningful days past learning")
+                .authorId(3L)
                 .build();
     }
 }

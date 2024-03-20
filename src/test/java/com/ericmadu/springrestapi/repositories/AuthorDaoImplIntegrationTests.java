817d55c063132877867953cd2d6757b09a3a8616
Eric Madureira <ericmadu416@gmail.com>
2024-03-20 10:57:54 -0300

    refactor: setup Data JPA

---
+++ src/test/java/com/ericmadu/springrestapi/repositories/AuthorDaoImplIntegrationTests.java
@@ -0,0 +1,72 @@
+//package com.ericmadu.springrestapi.dao.impl;
+//
+//import com.ericmadu.springrestapi.TestDataUtil;
+//import com.ericmadu.springrestapi.domain.Author;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.DirtiesContext;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@SpringBootTest
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+//@ExtendWith(SpringExtension.class)
+//public class AuthorDaoImplIntegrationTests {
+//
+//    private AuthorDaoImpl underTest;
+//
+//    @Autowired
+//    public AuthorDaoImplIntegrationTests(AuthorDaoImpl underTest){
+//        this.underTest = underTest;
+//    }
+//
+//    @Test
+//    public void testThatAuthorCanBeCreatedAndRecalled() {
+//        Author author = TestDataUtil.createTestAuthorA();
+//        underTest.create(author);
+//        Optional<Author> result = underTest.findOne(author.getId());
+//        assertThat(result).isPresent();
+//        assertThat(result.get()).isEqualTo(author);
+//    }
+//
+//    @Test
+//    public void testThatMultipleAuthorsCanBeCreatedAndRecalled() {
+//        Author authorA = TestDataUtil.createTestAuthorA();
+//        underTest.create(authorA);
+//        Author authorB = TestDataUtil.createTestAuthorB();
+//        underTest.create(authorB);
+//        Author authorC = TestDataUtil.createTestAuthorC();
+//        underTest.create(authorC);
+//        List<Author> result = underTest.find();
+//        assertThat(result)
+//                .hasSize(3)
+//                .containsExactly(authorA, authorB, authorC);
+//    }
+//
+//    @Test
+//    public void testThatAuthorCanBeUpdated() {
+//        Author author = TestDataUtil.createTestAuthorA();
+//        underTest.create(author);
+//        author.setName("Master Engineer");
+//        author.setAge(35);
+//        underTest.update(author.getId(), author);
+//        Optional<Author> result = underTest.findOne(author.getId());
+//        assertThat(result).isPresent();
+//        assertThat(result.get()).isEqualTo(author);
+//    }
+//
+//    @Test
+//    public void testThatAuthorCanBeDeleted() {
+//        Author author = TestDataUtil.createTestAuthorA();
+//        underTest.create(author);
+//        underTest.delete(author.getId());
+//        Optional<Author> result = underTest.findOne(author.getId());
+//        assertThat(result).isEmpty();
+//    }
+//}

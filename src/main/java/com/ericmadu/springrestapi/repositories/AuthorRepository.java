c7be4d0d6f9d688e4648fa5baec06f85d220479a
Eric Madureira <ericmadu416@gmail.com>
2024-03-20 11:53:49 -0300

    feat: Author repository and tests

--- src/main/java/com/ericmadu/springrestapi/repositories/AuthorRepository.java
+++ src/main/java/com/ericmadu/springrestapi/repositories/AuthorRepository.java
@@ -1,4 +1,9 @@
 package com.ericmadu.springrestapi.repositories;
 
-public interface AuthorRepository {
+import com.ericmadu.springrestapi.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/test/java/com/ericmadu/springrestapi/repositories/AuthorRepositoryIntegrationTests.java
+++ src/test/java/com/ericmadu/springrestapi/repositories/AuthorRepositoryIntegrationTests.java
@@ -1,40 +1,40 @@
-//package com.ericmadu.springrestapi.dao.impl;
-//
-//import com.ericmadu.springrestapi.TestDataUtil;
-//import com.ericmadu.springrestapi.domain.Author;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//@ExtendWith(SpringExtension.class)
-//public class AuthorDaoImplIntegrationTests {
-//
-//    private AuthorDaoImpl underTest;
-//
-//    @Autowired
-//    public AuthorDaoImplIntegrationTests(AuthorDaoImpl underTest){
-//        this.underTest = underTest;
-//    }
-//
-//    @Test
-//    public void testThatAuthorCanBeCreatedAndRecalled() {
-//        Author author = TestDataUtil.createTestAuthorA();
-//        underTest.create(author);
-//        Optional<Author> result = underTest.findOne(author.getId());
-//        assertThat(result).isPresent();
-//        assertThat(result.get()).isEqualTo(author);
-//    }
-//
+package com.ericmadu.springrestapi.repositories;
+
+import com.ericmadu.springrestapi.TestDataUtil;
+import com.ericmadu.springrestapi.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ExtendWith(SpringExtension.class)
+public class AuthorRepositoryIntegrationTests {
+
+    private AuthorRepository underTest;
+
+    @Autowired
+    public AuthorRepositoryIntegrationTests(AuthorRepository underTest){
+        this.underTest = underTest;
+    }
+
+    @Test
+    public void testThatAuthorCanBeCreatedAndRecalled() {
+        Author author = TestDataUtil.createTestAuthorA();
+        underTest.save(author);
+        Optional<Author> result = underTest.findById(author.getId());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(author);
+    }
+
 //    @Test
 //    public void testThatMultipleAuthorsCanBeCreatedAndRecalled() {
 //        Author authorA = TestDataUtil.createTestAuthorA();
@@ -69,4 +69,4 @@
 //        Optional<Author> result = underTest.findOne(author.getId());
 //        assertThat(result).isEmpty();
 //    }
-//}
+}

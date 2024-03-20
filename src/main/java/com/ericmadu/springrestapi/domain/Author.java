5b3b70095ba64d527e3bcf609608700dfd227ffc
Eric Madureira <ericmadu416@gmail.com>
2024-03-20 11:16:03 -0300

    feat: Author entity

--- src/main/java/com/ericmadu/springrestapi/domain/Author.java
+++ src/main/java/com/ericmadu/springrestapi/domain/Author.java
@@ -1,5 +1,6 @@
 package com.ericmadu.springrestapi.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,11 +10,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "authors")
 public class Author {
 
     // Long defaults to null, while long defaults to 0.
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
     private Long id;
+
     private String name;
+
     private Integer age;
 
 }

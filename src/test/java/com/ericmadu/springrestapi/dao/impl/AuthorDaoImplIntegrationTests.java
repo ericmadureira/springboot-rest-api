205426a07cf98f574463114b95169007186507c3
Eric Madureira <ericmadu416@gmail.com>
2024-03-20 10:32:20 -0300

    test: Author testThatAuthorCanBeDeleted

--- src/test/java/com/ericmadu/springrestapi/dao/impl/AuthorDaoImplIntegrationTests.java
+++ src/test/java/com/ericmadu/springrestapi/dao/impl/AuthorDaoImplIntegrationTests.java
@@ -60,4 +60,13 @@ public class AuthorDaoImplIntegrationTests {
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(author);
     }
+
+    @Test
+    public void testThatAuthorCanBeDeleted() {
+        Author author = TestDataUtil.createTestAuthorA();
+        underTest.create(author);
+        underTest.delete(author.getId());
+        Optional<Author> result = underTest.findOne(author.getId());
+        assertThat(result).isEmpty();
+    }
 }

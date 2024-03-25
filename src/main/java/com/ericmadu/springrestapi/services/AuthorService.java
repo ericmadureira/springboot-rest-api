aa4f4bcc079e149d7e044bbd5edce4de8f253e64
Eric Madureira <ericmadu416@gmail.com>
2024-03-25 14:44:43 -0300

    feat: Author Service

---
+++ src/main/java/com/ericmadu/springrestapi/services/AuthorService.java
@@ -0,0 +1,9 @@
+package com.ericmadu.springrestapi.services;
+
+import com.ericmadu.springrestapi.domain.entities.AuthorEntity;
+
+public interface AuthorService {
+    AuthorEntity createAuthor(AuthorEntity authorEntity) {
+
+    }
+}

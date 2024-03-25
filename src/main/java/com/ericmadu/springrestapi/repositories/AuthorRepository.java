de1bfef855ac7534681d817ca249f41b6c78268a
Eric Madureira <ericmadu416@gmail.com>
2024-03-25 15:41:46 -0300

    fix: Author custom query findAuthorsWithAgeGreaterThan

--- src/main/java/com/ericmadu/springrestapi/repositories/AuthorRepository.java
+++ src/main/java/com/ericmadu/springrestapi/repositories/AuthorRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// CrudRepository takes <Entity, pkey type>.
 @Repository
 public interface AuthorRepository extends CrudRepository<AuthorEntity, Long> {
     Iterable<AuthorEntity> ageLessThan(int age);
 
     // HQL query example from JPA.
-    @Query("SELECT a FROM Author a WHERE a.age > ?1")
+    @Query("SELECT a FROM AuthorEntity a WHERE a.age > ?1")
     Iterable<AuthorEntity> findAuthorsWithAgeGreaterThan(int age);
 }

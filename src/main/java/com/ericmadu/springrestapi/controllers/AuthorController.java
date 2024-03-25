f112681975d6697cb331045b4759dc5906f8cd7b
Eric Madureira <ericmadu416@gmail.com>
2024-03-25 16:22:19 -0300

    feat: createAuthor return http status

--- src/main/java/com/ericmadu/springrestapi/controllers/AuthorController.java
+++ src/main/java/com/ericmadu/springrestapi/controllers/AuthorController.java
@@ -4,6 +4,8 @@ import com.ericmadu.springrestapi.domain.dto.AuthorDto;
 import com.ericmadu.springrestapi.domain.entities.AuthorEntity;
 import com.ericmadu.springrestapi.mappers.Mapper;
 import com.ericmadu.springrestapi.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +23,9 @@ public class AuthorController {
     }
 
     @PostMapping(path = "/authors")
-    public AuthorDto createAuthor(@RequestBody AuthorDto author) {
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto author) {
         AuthorEntity authorEntity = authorMapper.mapFrom(author);
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
-        return authorMapper.mapTo(savedAuthorEntity);
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
 }
